476f87c48b5e34c35f8bf1df28084cf23432da02
Debbi Litt <debbilitt@172-16-40-213.DYNAPOOL.NYU.EDU>
2018-04-05 19:11:44 -0400

    final project

--- laser/src/ofApp.cpp
+++ laser/src/ofApp.cpp
@@ -1,324 +1,371 @@
 #include "ofApp.h"
 
+ofFbo letterFillFbo;
+ofFbo backgroundFbo;
+
 //--------------------------------------------------------------
 void ofApp::setup(){
+    
+    numLines.addListener(this, &ofApp::numLinesChanged);
+    randGridSize.addListener(this, &ofApp::randGridSizeChanged);
+    
     mGui.setup("Control Booth");
-//    mJitterSlider.setup("Jittery Letters", 0.0f, 0.0f, 5.0f);
-    
-//    mJitterSlider.addListener(this, &ofApp::onChangeJitter);
-
-
+    mask.setup("Mask", false);
     randLines.setup("Random Lines", false);
+    numLines.setup("Number of Lines", 500, 0, 5000);
     grid.setup("Grid", false);
+    gridSize.setup("GridSize", 15.0, 5.0, 30.0);
     sqGrid.setup("Square Grid", false);
+    sqGridSize.setup("Square Grid Size", 15.0, 5.0, 30.0);
+    sqCellSize.setup("Square Cell Size", 0.5, 0.0, 1.0);
     diamond.setup("Diamonds", false);
+    diamondSize.setup("Diamond Size", 20.0, 10.0, 30.0);
+    randGrid.setup("Random Grid", false);
+    randGridSize.setup("Random Grid Size", 10, 5, 20);
     
-    
+    mGui.add(&mask);
     mGui.add(&randLines);
+    mGui.add(&numLines);
     mGui.add(&grid);
+    mGui.add(&gridSize);
     mGui.add(&sqGrid);
+    mGui.add(&sqGridSize);
+    mGui.add(&sqCellSize);
     mGui.add(&diamond);
-    mGui.add(&mJitterSlider);
-
-
+    mGui.add(&diamondSize);
+    mGui.add(&randGrid);
+    mGui.add(&randGridSize);
+    
     //load a custom font
     //the ttf file must be inside your bin/data directory
-    font.load("BreeSerif-Regular.ttf", 100, true, false, true);
+    font.load("BreeSerif-Regular.ttf", 500, true, false, true);
     
     savePDF = false;
-
-
+    
+    lines.clear();
+    
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    ofFill();
+    
+    for (int i = 0; i < 500; i++) {
+        ofPoint x = ofPoint(ofRandom(-ofGetWidth() * 5, ofGetWidth() * 5), ofRandom(-ofGetHeight() * 5, ofGetHeight() * 5));
+        ofPoint y = ofPoint(ofRandom(-ofGetWidth() * 5, ofGetWidth() * 5), ofRandom(-ofGetHeight() * 5, ofGetHeight() * 5));
+        lines.lineTo(x);
+    }
+    
+    int space = 10;
+    for (int x = 0; x < ofGetWidth(); x += space) {
+        for (int y = 0; y < ofGetHeight(); y += space) {
+            int r = int(ofRandom(0, 3));
+            if (r == 2) {
+                ofPoint p1 = ofPoint(x, y);
+                ofPoint p2 = ofPoint(x + space, y + space);
+                points.push_back(p1);
+                points.push_back(p2);
+            }
+            else if (r == 1) {
+                ofPoint p1 = ofPoint(x, y + space);
+                ofPoint p2 = ofPoint(x + space, y);
+                points.push_back(p1);
+                points.push_back(p2);
+            }
+            else continue;
+        }
+    }
+    
+    letterFillFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    letterFillFbo.begin();
+    ofClear(255, 255, 255, 0);
+    letterFillFbo.end();
+    
+    backgroundFbo.allocate(ofGetWidth(), ofGetHeight(), GL_RGBA);
+    backgroundFbo.begin();
+    ofClear(255, 255, 255, 0);
+    backgroundFbo.end();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     
- 
-    if( savePDF ){
-        ofBeginSaveScreenAsPDF("screenshot-"+ofGetTimestampString()+".pdf", false);
+    /* Start PDF Save */
+    if( savePDF ) ofBeginSaveScreenAsPDF("screenshot-"+ofGetTimestampString()+".pdf", false);
+    
+    /* Clear background FBO */
+    backgroundFbo.begin();
+    ofClear(255, 255, 255, 0);
+    backgroundFbo.end();
+    
+    /* Draw pattern onto background FBO */
+    backgroundFbo.begin();
+    
+    if(randLines) drawRandomLines();
+    if(grid) drawGridLines();
+    if(sqGrid) drawSquareGrid();
+    if (diamond) drawDiamondGrid();
+    if (randGrid) drawRandomGrid();
+    
+    backgroundFbo.end();
+    
+    /* Set mask. */
+    
+    if (mask) {
+        letterFillFbo.getTexture().setAlphaMask(backgroundFbo.getTexture());
+        letterFillFbo.draw(0, 0);
+    }
+    else {
+        backgroundFbo.draw(0, 0);
     }
     
-
+    /* Draw Polyline of Letters */
     
-    /* Draw random lines */
-    
+    string s = ofToString((char)toggledKey);
+    vector < ofTTFCharacter > fontPaths = font.getStringAsPoints(s);
+    ofRectangle s_bbox = font.getStringBoundingBox(s, 0, 0);
     ofPushMatrix();
-    if(randLines){
-
-        ofPolyline p;
-       
-        p.clear();
-        
-        ofSetLineWidth(1.0);
-        ofSetColor(0);
-        ofFill();
-
-        for (int i = 0; i < 100; i++) {
-            ofPoint x = ofPoint(ofRandom(50, ofGetWidth()), ofRandom(50, ofGetHeight()));
-            ofPoint y = ofPoint(ofRandom(50, ofGetWidth()), ofRandom(50, ofGetHeight()));
-        p.lineTo(x);
-            }
-    
-    }
-    ofPopMatrix();
-    
-    /* Draw grid lines*/
-    
-    if(grid){
-        ofSetLineWidth(1.0);
-        ofSetColor(0);
-        ofNoFill();
-        
-        for (int i = 0; i < ofGetWidth(); i += 13) {
-            for (int j = 0; j < ofGetHeight(); j += 13) {
-                ofDrawRectangle(i, j, 13, 13);
-            }
-        }
-    }
-    
-    
-    /* Draw rectangle grid */
-    
-    if(sqGrid) {
-       
-        ofSetLineWidth(1.0);
-        ofSetColor(0);
-        ofNoFill();
-        for (int i = 0; i < ofGetWidth(); i += 10) {
-            for (int j = 0; j < ofGetHeight(); j += 10) {
-                ofDrawRectangle(i, j, 5, 5);
-            }
-        }
-        
-    }
-    
-    
-    
-    /* Draw triangular lines*/
-    
-    if(diamond){
-        ofSetLineWidth(1.0);
-        ofSetColor(0);
-        ofNoFill();
-        
-    ofPushMatrix();
-        ofTranslate(300,0);
-        ofRotate(45);
-        
-        for (int i = -500; i < ofGetHeight() + 500; i += 20) {
-            ofDrawLine(-500, i, ofGetWidth() + 500, i);
-        }
-    ofPopMatrix();
-        
-    ofPushMatrix();
-            ofTranslate(0, -300);
-            ofRotate(-45);
-        
-            for (int i = -500; i < ofGetHeight() + 500; i += 20) {
-            ofDrawLine(-500, i, ofGetWidth() + 500, i);
-            }
-    ofPopMatrix();
-    
-        
-    }
-    
-    
-/*Draw Polyline of Letters*/
-    
-ofPushMatrix();
-    
-    ofTranslate(ofGetWidth()/2, ofGetHeight()/2);
-    
+    ofTranslate(ofGetWidth() / 2 - s_bbox.width / 2, ofGetHeight() / 2 + s_bbox.height / 2);
     ofFill();
-    ofSetColor(ofColor::white);
+    ofSetColor(ofColor::black);
+    ofSetLineWidth(3.0);
+    for (int i = 0; i < fontPaths.size(); i++) {
+        vector <ofPolyline> polylines = fontPaths[i].getOutline();
+        for (int j = 0; j < polylines.size(); j++) {
+            polylines[j].draw();
+        }
+    }
+    ofPopMatrix();
     
-    vector < ofTTFCharacter > iPaths = font.getStringAsPoints("I");
-    vector < ofTTFCharacter > heartPaths = font.getStringAsPoints("<3");
-    vector < ofTTFCharacter > dPaths = font.getStringAsPoints("D");
-    vector < ofTTFCharacter > mPaths = font.getStringAsPoints("M");
-    
-/* letter I */
-   
-        ofPushMatrix();
-            ofTranslate(50, 0);
-                for (int i = 0; i < iPaths.size(); i++) {
-    
-                    // for every character break it out to polylines
-    
-                    vector <ofPolyline> polylines = iPaths[i].getOutline();
-    
-                    for (int j = 0; j < polylines.size(); j++) {
-                        polylines[j].draw();
-                    }
-                }
-        ofPopMatrix();
-    
- /* heart */
-    
-        ofPushMatrix();
-            ofTranslate(125, 0);
-                for (int i = 0; i < heartPaths.size(); i++) {
- 
-                    vector <ofPolyline> polylines = heartPaths[i].getOutline();
-                    
-                    for (int j = 0; j < polylines.size(); j++) {
-                        polylines[j].draw();
-                    }
-                }
-        ofPopMatrix();
-    
- /* letter D */
-    
-        ofPushMatrix();
-            ofTranslate(50, 125);
-                for (int i = 0; i < dPaths.size(); i++) {
-        
-                    vector <ofPolyline> polylines = dPaths[i].getOutline();
-        
-                    for (int j = 0; j < polylines.size(); j++) {
-                        polylines[j].draw();
-                    }
-                }
-    
-        ofPopMatrix();
-    
-/* letter M */
-    
-        ofPushMatrix();
-            ofTranslate(150, 125);
-            for (int i = 0; i < mPaths.size(); i++) {
-            
-                vector <ofPolyline> polylines = mPaths[i].getOutline();
-        
-                for (int j = 0; j < polylines.size(); j++) {
-                    polylines[j].draw();
-                }
-            }
-        ofPopMatrix();
-
-ofPopMatrix();
-    
-
-
+    /* End PDF Save*/
     if( savePDF ){
         ofEndSaveScreenAsPDF();
         savePDF = false;
     }
-
     
-        mGui.draw();
-
-    
+    /* Draw GUI */
+    mGui.draw();
 }
 
+/* BACKGROUND PATTERNS */
 
+void ofApp::drawRandomLines() {
+    ofPushMatrix();
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    lines.draw();
+    ofPopMatrix();
+}
 
-//--------------------------------------------------------------
-void ofApp::onChangeJitter(float & newJitterValue){
-    jitter.clear();
-    // get the string as paths
-    vector < ofTTFCharacter > paths = font.getStringAsPoints("I <3 DM");
+void ofApp::drawGridLines() {
+    ofPushMatrix();
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    ofNoFill();
     
-    for (int i = 0; i < paths.size(); i++){
-        
-        // for every character break it out to polylines
-        
-        vector <ofPolyline> polylines = paths[i].getOutline();
-        
-        //looping through polylines
-        
-        for (int j = 0; j < polylines.size(); j++){
-            // for (int k = 0; k < polylines[j].size(); k++){
-            //  ofDrawCircle( polylines[j][k], 1);
-            
-   //resampling polylines to space out along shape of letter
-            ofPolyline p = polylines[j].getResampledBySpacing(5);
-            
-            vector <ofPoint> points = p.getVertices();
-            
-            ofLog(OF_LOG_NOTICE, ofToString(points.size()));
-            
-            for (auto &v : points) {
-                v.x+=ofRandom(-newJitterValue,newJitterValue);
-                v.y+=ofRandom(-newJitterValue,newJitterValue);
-                
-                jitter.lineTo(v);
-                
-            }
-            
-            jitter.close();
+    for (int i = 0; i < ofGetWidth(); i += gridSize) {
+        for (int j = 0; j < ofGetHeight(); j += gridSize) {
+            ofDrawRectangle(i, j, gridSize, gridSize);
         }
     }
-
-    
+    ofPopMatrix();
 }
 
+void ofApp::drawSquareGrid() {
+    ofPushMatrix();
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    ofNoFill();
+    for (int i = 0; i < ofGetWidth(); i += sqGridSize) {
+        for (int j = 0; j < ofGetHeight(); j += sqGridSize) {
+            ofDrawRectangle(i, j, sqGridSize * sqCellSize, sqGridSize * sqCellSize);
+        }
+    }
+    ofPopMatrix();
+}
+
+void ofApp::drawRandomGrid() {
+    ofPushMatrix();
+    for (int i = 0; i < points.size(); i += 2) {
+        ofPoint pt1 = points.at(i);
+        ofPoint pt2 = points.at(i + 1);
+        ofSetLineWidth(1.0);
+        ofLine(pt1, pt2);
+    }
+    ofPopMatrix();
+}
+
+void ofApp::drawDiamondGrid() {
+    ofPushMatrix();
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    ofNoFill();
+    ofPushMatrix();
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+    ofRotate(45);
+    ofTranslate(-ofGetWidth() / 2, -ofGetHeight() / 2);
+    for (int i = 0; i < ofGetHeight(); i += diamondSize) {
+        ofDrawLine(0, i, ofGetWidth(), i);
+    }
+    ofPopMatrix();
+    ofPushMatrix();
+    ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+    ofRotate(-45);
+    ofTranslate(-ofGetWidth() / 2, -ofGetHeight() / 2);
+    for (int i = 0; i < ofGetHeight(); i += diamondSize) {
+        ofDrawLine(0, i, ofGetWidth(), i);
+    }
+    ofPopMatrix();
+    ofPopMatrix();
+}
+
+/* Callback Functions for Pattern Changes    */
+void ofApp::randGridSizeChanged(int & newRandGridSize) {
+    points.clear();
+    
+    int space = newRandGridSize;
+    for (int x = 0; x < ofGetWidth(); x += space) {
+        for (int y = 0; y < ofGetHeight(); y += space) {
+            int r = int(ofRandom(0, 3));
+            if (r == 2) {
+                ofPoint p1 = ofPoint(x, y);
+                ofPoint p2 = ofPoint(x + space, y + space);
+                points.push_back(p1);
+                points.push_back(p2);
+            }
+            else if (r == 1) {
+                ofPoint p1 = ofPoint(x, y + space);
+                ofPoint p2 = ofPoint(x + space, y);
+                points.push_back(p1);
+                points.push_back(p2);
+            }
+            else continue;
+        }
+    }
+}
+
+void ofApp::numLinesChanged(int & newNumLines) {
+    
+    lines.clear();
+    
+    ofSetLineWidth(1.0);
+    ofSetColor(0);
+    ofFill();
+    
+    for (int i = 0; i < newNumLines; i++) {
+        ofPoint x = ofPoint(ofRandom(-ofGetWidth() * 5, ofGetWidth() * 5), ofRandom(-ofGetHeight() * 5, ofGetHeight() * 5));
+        ofPoint y = ofPoint(ofRandom(-ofGetWidth() * 5, ofGetWidth() * 5), ofRandom(-ofGetHeight() * 5, ofGetHeight() * 5));
+        lines.lineTo(x);
+    }
+}
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
     
-        if(key == 's') {
+    if(key == '1') {
+        savePDF = true;
+        ofEndSaveScreenAsPDF();
+    }
     
-            savePDF = true;
-            ofEndSaveScreenAsPDF();
-           
+    if (key != '1') {
+        toggledKey = key;
+        string s = ofToString((char)key);
+        vector < ofTTFCharacter > fontPaths = font.getStringAsPoints(s);
+        ofRectangle s_bbox = font.getStringBoundingBox(s, 0, 0);
+        letterFillFbo.begin();
+        ofClear(255, 255, 255, 0);
+        letterFillFbo.end();
+        
+        letterFillFbo.begin();
+        ofPushMatrix();
+        ofTranslate(ofGetWidth() / 2 - s_bbox.width / 2, ofGetHeight() / 2 + s_bbox.height / 2);
+        
+        ofPath path;
+        for (int i = 0; i < fontPaths.size(); i++) {
+            vector <ofPolyline> polylines = fontPaths[i].getOutline();
+            for (int j = 0; j < polylines.size(); j++) {
+                polylines[j].draw();
+                for (int i = 0; i <  polylines[j].getVertices().size(); i++) {
+                    if (i == 0) {
+                        path.newSubPath();
+                        path.moveTo(polylines[j].getVertices()[i]);
+                    }
+                    else {
+                        path.lineTo(polylines[j].getVertices()[i]);
+                    }
+                }
+                path.close();
+                path.simplify();
+            }
         }
-    
-
+        path.setStrokeWidth(5.0);
+        path.setStrokeColor(ofColor::black);
+        path.draw();
+        
+        ofFill();
+        ofSetColor(ofColor::black);
+        ofSetLineWidth(3.0);
+        for (int i = 0; i < fontPaths.size(); i++) {
+            vector <ofPolyline> polylines = fontPaths[i].getOutline();
+            for (int j = 0; j < polylines.size(); j++) {
+                polylines[j].draw();
+            }
+        }
+        ofPopMatrix();
+        letterFillFbo.end();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
-
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- laser/src/ofApp.h
+++ laser/src/ofApp.h
@@ -4,43 +4,62 @@
 #include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-        void onChangeJitter(float & newJitterValue);
-  
     
-        ofTrueTypeFont font;
-        ofxPanel mGui;
-        ofxFloatSlider mJitterSlider;
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void numLinesChanged(int & newNumLines);
+    void randGridSizeChanged(int & newRandGridSize);
+    
+    void drawRandomLines();
+    void drawGridLines();
+    void drawSquareGrid();
+    void drawRandomGrid();
+    void drawDiamondGrid();
+    
+    ofTrueTypeFont font;
+    ofxPanel mGui;
+    ofxFloatSlider mJitterSlider;
+    
+    int toggledKey;
+    
+    ofxToggle mask;
     
     
-   //four sliders controlling each letter
-        ofxToggle randLines;
-        ofxToggle grid;
-        ofxToggle sqGrid;
-        ofxToggle diamond;
+    //four sliders controlling each letter
+    ofxToggle randLines;
+    ofxIntSlider numLines;
+    ofxToggle grid;
+    ofxFloatSlider gridSize;
+    ofxToggle sqGrid;
+    ofxFloatSlider sqGridSize;
+    ofxFloatSlider sqCellSize;
+    ofxToggle diamond;
+    ofxFloatSlider diamondSize;
+    ofxToggle randGrid;
+    ofxIntSlider randGridSize;
     
-        ofPolyline jitter;
+    ofPolyline lines;
+    
+    vector<ofPoint> points;
     
     bool savePDF;
     
     bool drawGrid;
     
-
-		
+    
+    
 };
